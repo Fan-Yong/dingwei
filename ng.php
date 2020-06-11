93b17e17f5d85fe91ccb30a42cf11d60d57b5eaf
Gao Zhen <buzhinanbei@gmail.com>
2020-06-11 12:23:01 +0800

    调整字体

--- ng.php
+++ ng.php
@@ -21,19 +21,18 @@ $pos2=$lng.",".$lat;
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	
-	<script type="text/javascript" src="//api.map.baidu.com/api?v=2.0&ak=yNAbC56QOZtHaG1xtFTaKUWr7lGpIVIb"></script>
 	<title>浏览器定位</title>
+	<style> 
+		body{ text-align:center} 
+		.div{ margin:0 auto; width:400px; height:500px; border:0px solid #F00} 
+		span {font-size: 3em;}
+	</style> 
 </head>
-<style> 
-body{ text-align:center} 
-.div{ margin:0 auto; width:400px; height:500px; border:0px solid #F00} 
-span {font-size: 5em;}
-</style> 
-<body>
-	<div class="div"><br><br><br><br><span id="span"><a href="http://api.map.baidu.com/marker?location=<?php echo $pos2  ?>&title=亲人位置&content=点击左下角'到这去',并根据提示打开百度app&output=html">点此导航</a></span></div>
-</body>
+	<body>
+	<div class="div"><br><br><br><br><span id="span">请稍后，正在打开百度...</span></div>
+	</body>
 </html>
+
 <script>
 function is_weixn(){
     var ua = navigator.userAgent.toLowerCase();
@@ -48,7 +47,7 @@ if(is_weixn()){
 	document.getElementById("span").innerHTML="请点击右上角'...',选择在浏览器打开";
 	
 }else{
-	//alert("aaa");
+	window.location.href="http://api.map.baidu.com/marker?location=<?php echo $pos2  ?>&title=亲人位置&content=点击左下角'到这去',并根据提示打开百度app&output=html";
 }
 	
 </script>
